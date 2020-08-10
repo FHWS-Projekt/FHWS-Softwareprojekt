9d411072fdbe71a5896be9ac924a48b4919b2acb
Hakan Arda <50369789+ArdaHakan@users.noreply.github.com>
2020-08-10 23:20:11 +0200

    Add Earth; Add TimeCycle

---
+++ Heal it!/Assets/Scripts/TimeCycle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeCycle : MonoBehaviour
+{
+    [SerializeField, Range(0, 24)] private float TimeOfDay;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Application.isPlaying)
+        {
+            TimeOfDay += Time.deltaTime;
+            TimeOfDay %= 24;
+        }
+
+
+        if(TimeOfDay <= 23)
+        {
+
+        }
+    }
+}
