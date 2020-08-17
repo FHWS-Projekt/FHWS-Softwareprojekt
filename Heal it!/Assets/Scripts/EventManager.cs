86d8e7063de807a96b62b141680408d399f8a421
Hakan Arda <50369789+ArdaHakan@users.noreply.github.com>
2020-08-17 03:25:06 +0200

    Add Continents;

---
+++ Heal it!/Assets/Scripts/EventManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    public Country[] countries;
+
+    public void RandomStart()
+    {
+        int rdm = (int)Random.Range(0.0f, 24.0f);
+        countries[rdm].infected = 1;
+        Debug.Log("Start country: " + countries[rdm].countryName);
+    }
+}
