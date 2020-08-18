5b5afc35d465f7a12fe5fe6e6c3607d1d43be39b
rabbito1995 <49904771+rabbito1995@users.noreply.github.com>
2020-08-18 12:24:37 +0200

    Add Intro-Text Pop-Up

--- Heal it!/Assets/Scripts/MainMenu.cs
+++ Heal it!/Assets/Scripts/MainMenu.cs
@@ -1,10 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject Panel;
+    private Text playerText;
+
+    void Awake()
+    {
+        playerText = GameObject.Find("PlayerText").GetComponent<Text>();
+    }
+
+    void Start()
+    {
+       if (playerText.text.Equals(""))
+        {
+            openPanel();
+        }
+    }
+
+    public void closePanel()
+    {
+        if (Panel != null)
+        {
+            Panel.SetActive(false);
+        }
+    }
+
+    public void openPanel()
+    {
+        if (Panel != null)
+        {
+            Panel.SetActive(true);
+        }
+    }
+
     public void playGame()
     {
         SceneManager.LoadScene("Main");
