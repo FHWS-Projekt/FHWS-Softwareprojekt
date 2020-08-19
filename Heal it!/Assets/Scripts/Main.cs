bca8ea212c165967f91cc22ca9d054702fe7fb17
Andreas <henneberger.am@gmail.com>
2020-08-19 13:07:16 +0200

    Add Time and Earth rotation

---
+++ Heal it!/Assets/Scripts/Main.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour
+{
+    #region Attributes
+    [SerializeField] protected GameObject earth;
+
+    // Time
+    protected MyDateTime myDateTime;
+    //protected float timestamp;
+    [SerializeField] protected TextMeshProUGUI timeDisplay;
+    [SerializeField] protected Button[] timeButtons = new Button[3];
+    #endregion Attributes
+
+    #region Getter and Setter
+    public GameObject Earth
+    {
+        get { return earth; }
+        set { earth = value; }
+    }
+
+    // Time
+    public MyDateTime MyDateTime {
+        get { return myDateTime; }
+        set { myDateTime = value; }
+    }
+    /*public float Timestamp
+    {
+        get { return timestamp; }
+        set { timestamp = value; }
+    }*/
+    public TextMeshProUGUI TimeDisplay
+    {
+        get { return timeDisplay; }
+        set { timeDisplay = value; }
+    }
+    public Button[] TimeButtons
+    {
+        get { return timeButtons; }
+        set { timeButtons = value; }
+    }
+    #endregion Getter and Setter
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Adds OnClick Listener to Buttons
+        SetTimeButtonsOnClick();
+
+        // Get Unix Timestamp in Seconds if it is not already set
+        if(MyDateTime == null) {
+            MyDateTime = new MyDateTime();
+            MyDateTime.Year = System.DateTime.Now.Year;
+            MyDateTime.Month = System.DateTime.Now.Month;
+            MyDateTime.Day = System.DateTime.Now.Day;
+            MyDateTime.Hour = System.DateTime.Now.Hour;
+        }
+
+
+        /*if (Timestamp == 0)
+        {
+            Timestamp = DateTimeOffset.Now.ToUnixTimeSeconds();
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateTimestamp();
+        RotateEarth();
+    }
+
+    #region Time
+    // Methods for managing Time
+    public void UpdateTimestamp()
+    {
+        //Timestamp += Time.deltaTime;
+        //TimeDisplay.text = Time.deltaTime.ToString();
+        MyDateTime.Hour += Time.deltaTime;
+        TimeDisplay.text = MyDateTime.toString();
+    }
+    public void SetTimeButtonsOnClick()
+    {
+        foreach (Button button in TimeButtons)
+        {
+            button.onClick.AddListener(() => SetTimeButtonsOnClickTask(button));
+        }
+    }
+    public void SetTimeButtonsOnClickTask(Button button)
+    {
+        int indexOfButton = System.Array.IndexOf(TimeButtons, button);
+        Time.timeScale = (float)Math.Pow(indexOfButton, 6);
+    }
+    #endregion Time
+
+
+    public void RotateEarth()
+    {
+        //Earth.transform.Rotate(Vector3.up, 10 * Time.deltaTime, Space.World);
+        Earth.transform.rotation = Quaternion.AngleAxis((float)(MyDateTime.Hour * 15), Vector3.up);
+    }
+}
---
+++ Heal it!/Assets/Scripts/MyDateTime.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class MyDateTime {
+    #region Attributes
+    private double hour;
+    private double day;
+    private double month;
+    private double year;
+    #endregion Attributes
+
+    #region Getter and Setter
+    public double Hour {
+        get { return hour; }
+        set { hour = value; }
+    }
+    public double Day {
+        get { return day; }
+        set { day = value; }
+    }
+    public double Month {
+        get { return month; }
+        set { month = value; }
+    }
+    public double Year {
+        get { return year; }
+        set { year = value; }
+    }
+    #endregion Getter and Setter
+
+    public void CalculateDateTime() {
+        if (Hour >= 24) {
+            Hour = Hour % 24;
+            Day += (int)(Day / 24 + 1);
+        } else if (Day >= CalculateMonthDays(Month)) {
+            int monthDays = CalculateMonthDays(Month);
+            Day = Day % monthDays;
+            Month += (int)(Day / monthDays);
+        } else if (Month >= 12) {
+            Month = Month % 12;
+            Year += (int)(Month / 12);
+        }
+    }
+
+    public int CalculateMonthDays(double month) {
+        switch (month) {
+            case 2: {
+                // February
+                return 28;
+            }
+            case 4: {
+                // April
+                return 30;
+            }
+            case 6: {
+                // Juni
+                return 30;
+            }
+            case 9: {
+                // September
+                return 30;
+            }
+            case 11: {
+                // November
+                return 30;
+            }
+            default: {
+                // All other Month
+                return 31;
+            }
+        }
+    }
+    public string toString() {
+        CalculateDateTime();
+        string year = Year < 10 ? "0" + Year : "" + Year;
+        string month = Month < 10 ? "0" + Month : "" + Month;
+        string day = Day < 10 ? "0" + Day : "" + Day;
+        return year + "-" + month + "-" + day + " " + (int)Hour + " Uhr";
+    }
+}
