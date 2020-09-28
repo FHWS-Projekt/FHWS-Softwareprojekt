06cd29f4b93f135066b00db473cc48e8e47526b4
ArdaHakan <50369789+ArdaHakan@users.noreply.github.com>
2020-09-28 16:48:04 +0200

    Add ButtonAnimatior;

---
+++ Heal it!/Assets/Scripts/ButtonAnimaitor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonAnimaitor : MonoBehaviour
+{
+
+    public Sprite pause;
+    public Sprite play;
+    public bool mode;
+    public Image buttonImage;
+
+    private void Start()
+    {
+        buttonImage = gameObject.GetComponent<Image>();
+        mode = true;
+
+    }
+
+    public void ChangeSprite()
+    {
+        if (mode)
+        {
+            mode = false;
+            buttonImage.sprite = play;
+        }
+        else if (!mode)
+        {
+            mode = true;
+            buttonImage.sprite = pause;
+        }
+    }
+}
