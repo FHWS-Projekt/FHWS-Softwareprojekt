8ac1641a835872a299851f635aee867cd3185c24
Andreas <henneberger.am@gmail.com>
2020-12-16 13:49:33 +0100

    Fixed Graph Bug Add JsonDatabasse

--- Heal it!/Assets/Scripts/ForceLandscape.cs
+++ Heal it!/Assets/Scripts/ForceLandscape.cs
@@ -1,13 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System.IO;
 
 public class ForceLandscape : MonoBehaviour
 {
+
+    [SerializeField] protected TextMeshProUGUI introTextTMP;
+    public static IntroTextData introTextData;
+    protected static readonly string myDatabase = Path.Combine(Application.streamingAssetsPath, "MyDatabase.json");
+
+
+    public class IntroTextData {
+        public string introText;
+    }
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         Screen.orientation = ScreenOrientation.LandscapeLeft;
+
+        introTextData = new JsonParser().ReadFromJson<IntroTextData>(myDatabase);
+        //introTextTMP.text = introTextData.introText;
     }
 
 
