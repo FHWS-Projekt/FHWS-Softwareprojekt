e29dc8421a72b19d411990e9a563940175e6847f
Andreas <henneberger.am@gmail.com>
2021-02-20 17:24:28 +0100

    Format JsonParser

--- Heal it!/Assets/Scripts/JsonParser.cs
+++ Heal it!/Assets/Scripts/JsonParser.cs
@@ -4,41 +4,50 @@ using UnityEngine;
 using System.IO;
 using System;
 
-public class JsonParser {
+public class JsonParser
+{
 
 
-    public T ReadFromJson<T>(string path) {
+    public T ReadFromJson<T>(string path)
+    {
         string elementJson = File.ReadAllText(path);
         return JsonUtility.FromJson<T>(elementJson);
     }
 
-    public void WriteToJson<T>(T element, string path) {
+    public void WriteToJson<T>(T element, string path)
+    {
         string elementJson = JsonUtility.ToJson(element, true);
         File.WriteAllText(path, elementJson);
     }
 
-    public T ReadFromJsonOS<T>(string path) {
+    public T ReadFromJsonOS<T>(string path)
+    {
         T data = Activator.CreateInstance<T>();
 
-        try {
+        try
+        {
             string dataAsJson = string.Empty;
-            if (Application.platform == RuntimePlatform.WindowsEditor) {
+            if(Application.platform == RuntimePlatform.WindowsEditor)
+            {
                 dataAsJson = File.ReadAllText(path);
             }
-            if (Application.platform == RuntimePlatform.Android) {
+            if(Application.platform == RuntimePlatform.Android)
+            {
 #pragma warning disable 0618
                 WWW reader = new WWW(path);
-                while (!reader.isDone) { }
+                while(!reader.isDone) { }
 
                 dataAsJson = reader.text;
 #pragma warning restore 0618
             }
-            if (Application.platform == RuntimePlatform.IPhonePlayer) {
+            if(Application.platform == RuntimePlatform.IPhonePlayer)
+            {
                 dataAsJson = File.ReadAllText(path);
             }
             data = JsonUtility.FromJson<T>(dataAsJson);
 
-        } catch (FileNotFoundException) {
+        } catch(FileNotFoundException)
+        {
             Debug.Log("QuizData not found at: \n" + path);
         }
 
