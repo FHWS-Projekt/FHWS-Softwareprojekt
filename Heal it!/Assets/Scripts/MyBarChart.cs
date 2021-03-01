72c0741235cad9ae7dbc4ea512ddf876b16eeb5a
Andreas <henneberger.am@gmail.com>
2021-03-01 18:06:45 +0100

    Format BarChart

--- Heal it!/Assets/Scripts/MyBarChart.cs
+++ Heal it!/Assets/Scripts/MyBarChart.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MyBarChart : MonoBehaviour {
+public class MyBarChart : MonoBehaviour
+{
 
     // Graph Objects
     [SerializeField] private RectTransform graphContainer;
@@ -20,57 +21,69 @@ public class MyBarChart : MonoBehaviour {
     private List<GameObject> gameObjectList;
     private List<BarChartVisual.BarChartVisualObject> graphVisualObjectList;
 
-    private void Awake() {
+    private void Awake()
+    {
         // Grab base objects references
         gameObjectList = new List<GameObject>();
         graphVisualObjectList = new List<BarChartVisual.BarChartVisualObject>();
     }
 
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
         Main.Instance.MyDateTime.DayTasks.Add(() => UpdateGraph());
         Main.Instance.MyDateTime.DayTasks.Add(() => newDay());
     }
 
-    public void newDay() {
+    public void newDay()
+    {
         ShowGraph();
         ShowGraph();
     }
 
-    private void UpdateGraph() {
+    private void UpdateGraph()
+    {
         int infected = 0;
-        foreach (Country country in eventManager.infectedCountries) {
+        foreach(Country country in eventManager.infectedCountries)
+        {
             infected = (int)(infected + country.infected);
         }
         double day = Main.Instance.MyDateTime.Day;
-        if(day > 1) {
+        if(day > 1)
+        {
             infectedList.Add(infected);
         }
     }
 
-    public void ShowGraph() {
-        if (!graphContainer.gameObject.activeSelf) {
+    public void ShowGraph()
+    {
+        if(!graphContainer.gameObject.activeSelf)
+        {
             graphContainer.gameObject.SetActive(true);
             BarChartVisual barChartVisual = new BarChartVisual(graphContainer, Color.yellow, .8f);
             ShowGraph(barChartVisual, (int _i) => "Day " + (_i + 1), (float _f) => "" + Mathf.RoundToInt(_f));
-        } else {
+        } else
+        {
             graphContainer.gameObject.SetActive(false);
         }
     }
-    
-    private void ShowGraph(BarChartVisual graphVisual, Func<int, string> getAxisLabelX = null, Func<float, string> getAxisLabelY = null) {
+
+    private void ShowGraph(BarChartVisual graphVisual, Func<int, string> getAxisLabelX = null, Func<float, string> getAxisLabelY = null)
+    {
 
         // Clean up previous graph
-        foreach (GameObject gameObject in gameObjectList) {
+        foreach(GameObject gameObject in gameObjectList)
+        {
             Destroy(gameObject);
         }
         gameObjectList.Clear();
 
-        foreach (BarChartVisual.BarChartVisualObject graphVisualObject in graphVisualObjectList) {
+        foreach(BarChartVisual.BarChartVisualObject graphVisualObject in graphVisualObjectList)
+        {
             graphVisualObject.CleanUp();
         }
         graphVisualObjectList.Clear();
-        
+
         // Grab the width and height from the container
         float graphWidth = graphContainer.sizeDelta.x;
         float graphHeight = graphContainer.sizeDelta.y;
@@ -79,15 +92,18 @@ public class MyBarChart : MonoBehaviour {
         float yMaximum = infectedList[0];
         float yMinimum = 0f; // Start the graph at zero
 
-        for (int i = 0; i < infectedList.Count; i++) {
+        for(int i = 0; i < infectedList.Count; i++)
+        {
             int value = infectedList[i];
-            if (value > yMaximum) {
+            if(value > yMaximum)
+            {
                 yMaximum = value;
             }
         }
 
         float yDifference = yMaximum - yMinimum;
-        if (yDifference <= 0) {
+        if(yDifference <= 0)
+        {
             yDifference = 5f;
         }
         yMaximum = yMaximum + (yDifference * 0.2f);
@@ -97,7 +113,8 @@ public class MyBarChart : MonoBehaviour {
 
         // Cycle through all visible data points
         int xIndex = 0;
-        for (int i = 0; i < infectedList.Count; i++) {
+        for(int i = 0; i < infectedList.Count; i++)
+        {
             float xPosition = xIndex * xSize + xSize;
             float yPosition = (infectedList[i] / yMaximum) * graphHeight;
 
@@ -111,7 +128,7 @@ public class MyBarChart : MonoBehaviour {
             labelX.anchoredPosition = new Vector2(xPosition, -7f);
             labelX.GetComponent<Text>().text = getAxisLabelX(i);
             gameObjectList.Add(labelX.gameObject);
-            
+
             // Duplicate the x dash template
             RectTransform dashX = Instantiate(dashTemplateX);
             dashX.SetParent(dashContainer, false);
@@ -124,7 +141,8 @@ public class MyBarChart : MonoBehaviour {
 
         // Set up separators on the y axis
         int separatorCount = 10;
-        for (int i = 0; i <= separatorCount; i++) {
+        for(int i = 0; i <= separatorCount; i++)
+        {
             // Duplicate the label template
             RectTransform labelY = Instantiate(labelTemplateY);
             labelY.SetParent(graphContainer, false);
@@ -144,28 +162,32 @@ public class MyBarChart : MonoBehaviour {
     }
 
     // Displays data points as a Bar Chart
-    private class BarChartVisual {
+    private class BarChartVisual
+    {
 
         private RectTransform graphContainer;
         private Color barColor;
         private float barWidthMultiplier;
 
-        public BarChartVisual(RectTransform graphContainer, Color barColor, float barWidthMultiplier) {
+        public BarChartVisual(RectTransform graphContainer, Color barColor, float barWidthMultiplier)
+        {
             this.graphContainer = graphContainer;
             this.barColor = barColor;
             this.barWidthMultiplier = barWidthMultiplier;
         }
 
-        public BarChartVisualObject CreateGraphVisualObject(Vector2 graphPosition, float graphPositionWidth) {
+        public BarChartVisualObject CreateGraphVisualObject(Vector2 graphPosition, float graphPositionWidth)
+        {
             GameObject barGameObject = CreateBar(graphPosition, graphPositionWidth);
 
             BarChartVisualObject barChartVisualObject = new BarChartVisualObject(barGameObject, barWidthMultiplier);
-            barChartVisualObject.SetGraphVisualObjectInfo(graphPosition, graphPositionWidth);
+            //barChartVisualObject.SetGraphVisualObjectInfo(graphPosition, graphPositionWidth);
 
             return barChartVisualObject;
         }
 
-        private GameObject CreateBar(Vector2 graphPosition, float barWidth) {
+        private GameObject CreateBar(Vector2 graphPosition, float barWidth)
+        {
             GameObject gameObject = new GameObject("bar", typeof(Image));
             gameObject.transform.SetParent(graphContainer, false);
             gameObject.GetComponent<Image>().color = barColor;
@@ -179,23 +201,27 @@ public class MyBarChart : MonoBehaviour {
             return gameObject;
         }
 
-        public class BarChartVisualObject {
+        public class BarChartVisualObject
+        {
 
             private GameObject barGameObject;
             private float barWidthMultiplier;
 
-            public BarChartVisualObject(GameObject barGameObject, float barWidthMultiplier) {
+            public BarChartVisualObject(GameObject barGameObject, float barWidthMultiplier)
+            {
                 this.barGameObject = barGameObject;
                 this.barWidthMultiplier = barWidthMultiplier;
             }
 
-            public void SetGraphVisualObjectInfo(Vector2 graphPosition, float graphPositionWidth) {
+            public void SetGraphVisualObjectInfo(Vector2 graphPosition, float graphPositionWidth)
+            {
                 RectTransform rectTransform = barGameObject.GetComponent<RectTransform>();
                 rectTransform.anchoredPosition = new Vector2(graphPosition.x, 0f);
                 rectTransform.sizeDelta = new Vector2(graphPositionWidth * barWidthMultiplier, graphPosition.y);
             }
 
-            public void CleanUp() {
+            public void CleanUp()
+            {
                 Destroy(barGameObject);
             }
         }
